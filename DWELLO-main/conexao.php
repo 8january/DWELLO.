b969c614692ebcb0b8cd17a1c5f74c3f72de2f60
= <leonardobrandaoamarante@gmail.com>
2023-09-16 07:44:47 -0400

    iniciais adicionadas

--- DWELLO-main/conexao.php
+++ DWELLO-main/conexao.php
@@ -1,11 +1,11 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "bddwello";
+    $AEL_host = "localhost";
+    $AEL_username = "root";
+    $AEL_password = "";
+    $AEL_db = "bddwello";
 
-    $conn = new mysqli($host, $username, $password, $db);
+    $AEL_conn = new mysqli($AEL_host, $AEL_username, $AEL_password, $AEL_db);
 
-    if($conn->connect_errno != 0)
-        echo "Falha de conexão: (".$conn->connect_errno.")";
+    if($AEL_conn->connect_errno != 0)
+        echo "Falha de conexão: (".$AEL_conn->connect_errno.")";
 ?>
--- DWELLO-main/index.php
+++ DWELLO-main/index.php
@@ -2,66 +2,66 @@
 include "conexao.php";
 
 if(isset($_GET['filter'])){
-    $key = $_GET['filter'];
-    $sql = "SELECT * FROM produtos where nome LIKE '$key%'";
+    $AEL_key = $_GET['filter'];
+    $AEL_sql = "SELECT * FROM produtos where nome LIKE '$AEL_key%'";
 }
 else{
-    $sql = "SELECT * FROM produtos";
+    $AEL_sql = "SELECT * FROM produtos";
 }
 
 if(isset($_GET['hasSubmitted'])){
-    $preco_l = $_GET['preco_l'];
-    $preco_r = $_GET['preco_r'];
-    $tamanho_l = $_GET['tamanho_l'];
-    $tamanho_r = $_GET['tamanho_r'];
-    $cor = $_GET['cor'];
-    $material = $_GET['material'];
-    $peso_l = $_GET['peso_l'];
-    $peso_r = $_GET['peso_r'];
+    $AEL_preco_l = $_GET['preco_l'];
+    $AEL_preco_r = $_GET['preco_r'];
+    $AEL_estoque_l = $_GET['estoque_l'];
+    $AEL_estoque_r = $_GET['estoque_r'];
+    $AEL_cor = $_GET['cor'];
+    $AEL_material = $_GET['material'];
+    $AEL_peso_l = $_GET['peso_l'];
+    $AEL_peso_r = $_GET['peso_r'];
     
-    $sql = "SELECT * FROM produtos";
+    $AEL_sql = "SELECT * FROM produtos";
     
-    if (!empty($preco_l) && !empty($preco_r)) {
-        $sql .= " WHERE valor BETWEEN $preco_l AND $preco_r";
+    if (!empty($AEL_preco_l) && !empty($AEL_preco_r)) {
+        $AEL_sql .= " WHERE valor BETWEEN $AEL_preco_l AND $AEL_preco_r";
     }
     
-    if (!empty($tamanho_l) && !empty($tamanho_r)) {
-        if (strpos($sql, 'WHERE') !== false) {
-            $sql .= " AND tamanho BETWEEN $tamanho_l AND $tamanho_r";
+    if (!empty($AEL_estoque_l) && !empty($AEL_estoque_r)) {
+        if (strpos($AEL_sql, 'WHERE') !== false) {
+            $AEL_sql .= " AND qnt_estoque BETWEEN $AEL_estoque_l AND $AEL_estoque_r";
         } else {
-            $sql .= " WHERE tamanho BETWEEN $tamanho_l AND $tamanho_r";
+            $AEL_sql .= " WHERE qnt_estoque BETWEEN $AEL_estoque_l AND $AEL_estoque_r";
         }
     }
     
-    if (!empty($cor)) {
-        if (strpos($sql, 'WHERE') !== false) {
-            $sql .= " AND cor = '$cor'";
+    if (!empty($AEL_cor)) {
+        if (strpos($AEL_sql, 'WHERE') !== false) {
+            $AEL_sql .= " AND cor = '$AEL_cor'";
         } else {
-            $sql .= " WHERE cor = '$cor'";
+            $AEL_sql .= " WHERE cor = '$AEL_cor'";
         }
     }
     
-    if (!empty($material)) {
-        if (strpos($sql, 'WHERE') !== false) {
-            $sql .= " AND material = '$material'";
+    if (!empty($AEL_material)) {
+        if (strpos($AEL_sql, 'WHERE') !== false) {
+            $AEL_sql .= " AND material = '$AEL_material'";
         } else {
-            $sql .= " WHERE material = '$material'";
+            $AEL_sql .= " WHERE material = '$AEL_material'";
         }
     }
     
-    if (!empty($peso_l) && !empty($peso_r)) {
-        if (strpos($sql, 'WHERE') !== false) {
-            $sql .= " AND peso BETWEEN $peso_l AND $peso_r";
+    if (!empty($AEL_peso_l) && !empty($AEL_peso_r)) {
+        if (strpos($AEL_sql, 'WHERE') !== false) {
+            $AEL_sql .= " AND peso BETWEEN $AEL_peso_l AND $AEL_peso_r";
         } else {
-            $sql .= " WHERE peso BETWEEN $peso_l AND $peso_r";
+            $AEL_sql .= " WHERE peso BETWEEN $AEL_peso_l AND $AEL_peso_r";
         }
     }
     }
 
-$result = mysqli_query($conn, $sql);
+$AEL_result = mysqli_query($AEL_conn, $AEL_sql);
 
-if (!$result) {
-    die("Erro na consulta: " . mysqli_error($conn));
+if (!$AEL_result) {
+    die("Erro na consulta: " . mysqli_error($AEL_conn));
 }
 
 ?>
@@ -129,20 +129,20 @@ if (!$result) {
             <th>Estoque</th>
             <th>Operações</th>
         </tr>
-        <?php while ($row = mysqli_fetch_assoc($result)): ?>
+        <?php while ($AEL_row = mysqli_fetch_assoc($AEL_result)): ?>
             <tr>
-                <td><?php echo $row['nome']; ?></td>
-                <td>R$ <?php echo $row['valor']; ?></td>
-                <td><?php echo $row['cor']; ?></td>
-                <td><?php echo $row['material']; ?></td>
-                <td><?php echo $row['formato']; ?></td>
-                <td><?php echo $row['qnt_estoque']; ?></td>
+                <td><?php echo $AEL_row['nome']; ?></td>
+                <td>R$ <?php echo $AEL_row['valor']; ?></td>
+                <td><?php echo $AEL_row['cor']; ?></td>
+                <td><?php echo $AEL_row['material']; ?></td>
+                <td><?php echo $AEL_row['formato']; ?></td>
+                <td><?php echo $AEL_row['qnt_estoque']; ?></td>
                 <td class='d-grid gap-2 d-md-block'>
-                        <a href="Inventory/sell.php?codigo=<?php echo $row['codigo']; ?>"><button class="btn btn-success btn-sm">Vender</button></a>
-                        <a href="Inventory/add.php?codigo=<?php echo $row['codigo']; ?>"><button class="btn btn-secondary btn-sm">Adicionar</button></a>
-                        <a href="CRUD/read.php?codigo=<?php echo $row['codigo']; ?>"><button class="btn btn-primary btn-sm">Detalhes</button></a>
-                        <a href="CRUD/update.php?codigo=<?php echo $row['codigo']; ?>"><button class="btn btn-warning btn-sm">Editar</button></a>
-                        <a href="CRUD/delete.php?codigo=<?php echo $row['codigo']; ?>"><button type="button" class="btn btn-outline-danger btn-sm">Excluir</button></a>
+                        <a href="Inventory/sell.php?codigo=<?php echo $AEL_row['codigo']; ?>"><button class="btn btn-success btn-sm">Vender</button></a>
+                        <a href="Inventory/add.php?codigo=<?php echo $AEL_row['codigo']; ?>"><button class="btn btn-secondary btn-sm">Adicionar</button></a>
+                        <a href="CRUD/read.php?codigo=<?php echo $AEL_row['codigo']; ?>"><button class="btn btn-primary btn-sm">Detalhes</button></a>
+                        <a href="CRUD/update.php?codigo=<?php echo $AEL_row['codigo']; ?>"><button class="btn btn-warning btn-sm">Editar</button></a>
+                        <a href="CRUD/delete.php?codigo=<?php echo $AEL_row['codigo']; ?>"><button type="button" class="btn btn-outline-danger btn-sm">Excluir</button></a>
                 </td>
             </tr>
         <?php endwhile; ?>
@@ -165,15 +165,6 @@ if (!$result) {
     <div class="col"><input type="number" name="preco_r" id="" placeholder='0.00' class='form-control form-control-sm' step="any"></div>
 </div>
 
-
-<div class="row row-cols-4 text-left align-items-center g-3 p-2">
-    <div class="col"><label for="" class='form-label'>Tamanho:</label></div>
-    <div class="col"><input type="number" name="tamanho_l" id="" placeholder='0.00' class='form-control form-control-sm' step="any"></div>
-    <div class="col"><label for="" class='form-label'>  entre </label></div>
-    <div class="col"><input type="number" name="tamanho_r" id="" placeholder='0.00' class='form-control form-control-sm' step="any"></div>
-</div>
-
-
 <div class="row row-cols-4 text-left align-items-center g-3 p-2">
     <div class="col"><label for="" class='form-label'>Cor:</label></div>
     <div class="col"><input type="text" name="cor" id="" placeholder='branco' class='form-control form-control-sm'></div>
@@ -189,6 +180,13 @@ if (!$result) {
     <div class="col"><input type="number" name="peso_r" id="" placeholder='0.00' class='form-control form-control-sm' step="any"></div>
 </div>
 
+<div class="row row-cols-4 text-left align-items-center g-3 p-2">
+    <div class="col"><label for="" class='form-label'>Estoque:</label></div>
+    <div class="col"><input type="number" name="estoque_l" id="" placeholder='0.00' class='form-control form-control-sm' step="any"></div>
+    <div class="col"><label for="" class='form-label'>  entre </label></div>
+    <div class="col"><input type="number" name="estoque_r" id="" placeholder='0.00' class='form-control form-control-sm' step="any"></div>
+</div>
+
 <input type="hidden" name="hasSubmitted" value="1">
 
 <div class="d-grid gap-2">
@@ -198,6 +196,5 @@ if (!$result) {
             </form>
         </div>
         </div>
-    <!--</div>-->
 </body>
 </html>
